286bb9afe63374c6c92c8db52abbd5cb7bbd9c52
ihamidz <151486755+ihamidz@users.noreply.github.com>
2023-11-20 14:15:28 +1100

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,49 +1,53 @@
+import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import ruptures as rpt
 
-# Step 3: Load your geochemical dataset
-data = pd.read_csv('NDI2.csv')
+def main():
+    st.title("Multivariate Change Point Detection App")
 
-# Step 4: Preprocess the data
-# Assuming the first column is the sample id and the rest are elements
-X = data.iloc[:, 1:].values  # Adjust the range to cover all 31 variables
+    # Step 1: Load your geochemical dataset
+    data = pd.read_csv('NDI2.csv')
 
-# Step 5: Standardize the data
-standardized_data = (X - np.mean(X, axis=0)) / np.std(X, axis=0)
+    # Step 2: Sidebar with user input
+    st.sidebar.header("Parameters")
+    pen_value = st.sidebar.slider("Penalty Term", min_value=1, max_value=100, value=50)
 
-# Step 6: Define weight scores for each variable
-weights = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1.5, 1.5, 1.5, 1.5,1.5, 1.5,1.5, 1.5,1.5]
+    # Step 3: Preprocess the data
+    X = data.iloc[:, 1:].values
 
-# Step 7: Apply weights to the standardized variables
-weighted_data = standardized_data * weights
+    # Step 4: Standardize the data
+    standardized_data = (X - np.mean(X, axis=0)) / np.std(X, axis=0)
 
-# Step 8: Perform change point detection on weighted data with automatic breakpoint selection using BIC
-model = "rbf"  # you can choose other models, e.g., "l2", "rbf", etc.
-algo = rpt.Pelt(model=model).fit(weighted_data)
-result = algo.predict(pen=1)  # adjust the penalty term as needed
+    # Step 5: Define weight scores for each variable
+    weights = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5]
 
-# Step 9: Visualize the results with a legend
-plt.figure(figsize=(10, 4))  # Adjust the figure size as needed
+    # Step 6: Apply weights to the standardized variables
+    weighted_data = standardized_data * weights
 
-# Plot each standardized and weighted element separately and label with element names
-for i in range(weighted_data.shape[1]):
-    plt.plot(weighted_data[:, i], label=data.columns[i + 1])
+    # Step 7: Perform change point detection on weighted data with automatic breakpoint selection using BIC
+    model = "rbf"
+    algo = rpt.Pelt(model=model).fit(weighted_data)
+    result = algo.predict(pen=pen_value)
 
-plt.title('Multivariate Change Point Detection (Standardized and Weighted Data)')
-plt.xlabel('Data Points')
-plt.ylabel('Weighted Values')
+    # Step 8: Visualize the results
+    plot_change_points(weighted_data, result, data.columns)
 
-# Mark the change points
-for bkp in result:
-    plt.axvline(bkp, linestyle='dashed', color='red')
+def plot_change_points(weighted_data, result, column_names):
+    plt.figure(figsize=(10, 4))
+    for i in range(weighted_data.shape[1]):
+        plt.plot(weighted_data[:, i], label=column_names[i + 1])
 
-# Add legend
-plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left')
+    plt.title('Multivariate Change Point Detection (Standardized and Weighted Data)')
+    plt.xlabel('Data Points')
+    plt.ylabel('Weighted Values')
 
-# Save the figure as a PNG file
-plt.savefig('standardized_weighted_change_point_detection_plot.png', bbox_inches='tight')
+    for bkp in result:
+        plt.axvline(bkp, linestyle='dashed', color='red')
 
-# Show the plot
-plt.show()
+    plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left')
+    st.pyplot()
+
+if __name__ == "__main__":
+    main()
